8ba109a13435fc9890e9c34935f4d6bfd746aa81
RyanA3 <alsobrooks.ryan@gmail.com>
2023-11-18 17:17:12 -0500

    Setup webgl rendering for game

--- public/launchgame/Launch!.js
+++ public/launchgame/Launch!.js
@@ -1,40 +1,35 @@
-var mat4;
-
+import * as mat4 from "../modules/esm/mat4.js";
 const vertShader = `
+#version 100
 
-attribute vec4 aPos;
+attribute vec3 aPos;
 attribute vec3 aColor;
 uniform mat4 modelViewMatrix;
 uniform mat4 projectionMatrix;
+varying vec3 oColor;
 
 void main() {
-    gl_Position = projectionMatrix * modelViewMatrix * aPos;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(aPos, 1);
+    oColor = aColor;
 }
 
 `;
 
 const fragShader = `
+#version 100
 
+precision highp float;
+varying vec3 oColor;
 
 void main() {
-    gl_FragColor = vec4(1);
+    gl_FragColor = vec4(oColor, 1.0);
 }
 
 `;
 
 main();
 
-function imports() {
-  var lmat4;
-  import("../modules/gl-matrix").then((mat4) => {
-    lmat4 = mat4;
-  });
-  mat4 = lmat4;
-}
-
-function main() {
-  imports();
-
+async function main() {
   const canvas = document.querySelector("#launchCanvas");
   const gl = canvas.getContext("webgl2");
 
@@ -52,17 +47,21 @@ function main() {
   const vbo = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
   const vertices = [
-    -0.5, -0.5, -1.0, 1, 0, 0, 0, 0.5, -1.0, 0, 1, 0, 0.5, -0.5, -1.0, 0, 0, 1,
+    -0.25, -0.25, -1.0, 1, 0, 0, 0, 0.25, -1.0, 0, 1, 0, 0.25, -0.25, -1.0, 0,
+    0, 1,
   ];
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
   //Configure the vertex attribute for position
   const posLoc = gl.getAttribLocation(shader, "aPos");
-  gl.vertexAttribPointer(posLoc, 3, gl.FLOAT, false, 6, 0);
+  gl.vertexAttribPointer(posLoc, 3, gl.FLOAT, false, 6 * 4, 0);
   gl.enableVertexAttribArray(posLoc);
-  //const colorLoc = gl.getAttribLocation(shader, "aColor");
-  //gl.vertexAttribPointer(colorLoc, 3, gl.FLOAT, false, 6, 3);
-  //gl.enableVertexAttribArray(colorLoc);
+  const colorLoc = gl.getAttribLocation(shader, "aColor");
+  gl.vertexAttribPointer(colorLoc, 3, gl.FLOAT, false, 6 * 4, 3 * 4);
+  gl.enableVertexAttribArray(colorLoc);
+
+  //Rendering setup
+  gl.useProgram(shader);
 
   //Initialize rendering settings
   gl.clearColor(0, 0, 0, 1);
@@ -75,8 +74,22 @@ function main() {
   const modelViewLoc = gl.getUniformLocation(shader, "modelViewMatrix");
   setCam(gl, 45, 0, 0, 0, projLoc, modelViewLoc);
 
-  //Render
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  var r = 0.0;
+  while (true) {
+    r++;
+
+    //Render
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    setCam(gl, 45, 0, 0, Math.sin(r / 100.0) - 1, projLoc, modelViewLoc);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 3);
+
+    await sleep(20);
+  }
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 function setCam(gl, fov, tx, ty, tz, projLoc, modelViewLoc) {
@@ -94,7 +107,7 @@ function setCam(gl, fov, tx, ty, tz, projLoc, modelViewLoc) {
 
   //Update uniforms
   gl.uniformMatrix4fv(projLoc, false, projectionMatrix);
-  gl.uniformMatrix4fv(modelViewLoc, false, modelViewLoc);
+  gl.uniformMatrix4fv(modelViewLoc, false, modelViewMatrix);
 }
 
 //Loads a vertex and fragment shader, compiles and links them
